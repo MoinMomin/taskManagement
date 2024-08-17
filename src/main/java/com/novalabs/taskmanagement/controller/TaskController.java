7bb3d134e2b54c5d47087b7224881a84669197a4
MoinMomin <89380085+MoinMomin@users.noreply.github.com>
2024-08-17 19:12:44 +0530

    Update TaskController.java

--- src/main/java/com/novalabs/taskmanagement/controller/TaskController.java
+++ src/main/java/com/novalabs/taskmanagement/controller/TaskController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/task")
+//@RequestMapping("/task")
 public class TaskController {
     @Autowired
     TaskService taskService;
